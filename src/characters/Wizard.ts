e8a54a50f878badf0a863385fbcdeb456ff505a0
ionchi <ion.chiriac@live.com>
2023-07-03 15:52:59 +0200

    feat: refactor AI assisted

--- src/characters/Wizard.ts
+++ src/characters/Wizard.ts
@@ -3,29 +3,33 @@ import { Game } from '../Game';
 import { Tool } from '../tools/Tool';
 
 export class Wizard extends Character {
-    private readonly GIFT_MESSAGE = 'You\'ve been given a gift!';
-    private readonly NO_MORE_GIFTS_MESSAGE = 'I don\'t have another gift for you!';
-    private readonly GIFT_RESPONSE = 'I\'ve given you a lighter version of your gift!';
+    private static readonly GIFT_MESSAGE = 'You\'ve been given a gift!';
+    private static readonly NO_MORE_GIFTS_MESSAGE = 'I don\'t have another gift for you!';
+    private static readonly GIFT_RESPONSE = 'I\'ve given you a lighter version of your gift!';
 
-    private gift: Tool;
+    private gift: Tool | null;
 
     constructor(name: string, welcomeMessage: string, tool: Tool) {
         super(name, welcomeMessage);
         this.gift = tool;
     }
+
     public override interact(game: Game): string {
-        if (this.gift) {
-            game.getPlayer().getBackpack().addTool(this.gift);
-            this.gift = null;
-            return this.GIFT_MESSAGE;
-        } else {
-            return this.NO_MORE_GIFTS_MESSAGE;
-        }
+        return this.gift ? this.giveGift(game) : Wizard.NO_MORE_GIFTS_MESSAGE;
+    }
+
+    private giveGift(game: Game): string {
+        game.getPlayer().getBackpack().addTool(this.gift);
+        this.gift = null;
+        return Wizard.GIFT_MESSAGE;
     }
 
     public receiveGift(game: Game, tool: Tool): string {
-        const lighterTool = new Tool(tool.getName(), tool.getWeight() / 2);
-        game.getCurrentRoom().addTool(lighterTool);
-        return this.GIFT_RESPONSE;
+        game.getCurrentRoom().addTool(this.createLighterTool(tool));
+        return Wizard.GIFT_RESPONSE;
+    }
+
+    private createLighterTool(tool: Tool): Tool {
+        return new Tool(tool.getName(), tool.getWeight() / 2);
     }
 }
